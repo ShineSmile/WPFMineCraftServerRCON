d3d579647820982283ef1966ff626003e394c8f3
Shine <ShineSmile@outlook.com>
2019-10-20 14:32:38 +0800

    update solution structure.

--- WPFMineCraftServerRCON/App.xaml.cs
+++ WPFMineCraftServerRCON/App.xaml.cs
@@ -1,11 +1,17 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace WPFMineCraftServerRCON
 {
-	/// <summary>
-	/// App.xaml 的交互逻辑
-	/// </summary>
-	public partial class App : Application
-	{
-	}
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
 }
